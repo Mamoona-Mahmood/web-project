1349954e7d03dcf223ff5a23679760cc508e2896
MamoonaMahmood <mamoona.mahmood-int@dubizzlelabs.com>
2025-05-01 15:16:16 +0500

    add client files

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('Server is updated!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
