4c61a6569f03fc66715e509239db4c3b8f6e2164
MamoonaMahmood <mamoona.mahmood-int@dubizzlelabs.com>
2025-05-04 17:59:42 +0500

    set up changed

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const colors = require("colors");
+const moragan = require("morgan");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+//dotenv conig
+dotenv.config();
+
+//mongodb connection
+connectDB();
+
+//rest obejct
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(moragan("dev"));
+
+//port
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(
+      `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
+        .bgCyan.white
+    );
+  });
